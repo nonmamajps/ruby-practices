53821bea7387d557fb99c3c688e45fa72827dcc7
nonmama <ig.non.noie@gmail.com>
2024-02-16 16:09:01 +0900

    書き直し

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,42 +1,54 @@
-#!/usr/bin/env ruby
-
-require 'optparse'
+# Dateクラス取り込む
 require 'date'
 
-options = {}
+# opt取り込む
+require 'optparse'
 opt = OptionParser.new
 
-opt.on("-y YEAR", Integer) do |year|
-    options[:year] = year
+#ハッシュに格納
+params = {}
+
+#opt.on('-m') {|m| params[:month] = m } #これと18行目のmonthは違うもの（do...endにしてみる）
+opt.on('-m') do |m|
+    params[:month] = m 
 end
+opt.on('-y') {|y| params[:year] = y }
 
-opt.on("-m MONTH", Integer) do |month|
-    options[:month] = month
-end
+opt.parse!
 
-opt.parse!(ARGV)
+today = Date.today
+year = params[:year] || Date.today.year 
+month = params[:month] || Date.today.month 
 
-year = options[:year] || Date.today.year
-month = options[:month] || Date.today.month
+#def print_calendar(year,month)
 
-def print_calendar(year, month)
-  first_day = Date.new(year, month, 1)
-  last_day = Date.new(year, month, -1)
+# 今月の最初の日
+first_day = Date.new(year, month, 1)
+# 今月の最後の日
+last_day = Date.new(year, month,-1)
 
-  puts "#{month}月 #{year}".center(20)
+# m月yを表示させる
+puts "#{month}月 #{year}".center(20)
 
-  wdays = ["日", "月", "火", "水", "木", "金", "土"]
-  puts wdays.join(" ")
+# 曜日を日本語で横並びにする
+puts "日 月 火 水 木 金 土"
 
-  print "   " * first_day.wday
+#　月初を合わせる（わからん）
+print "   " * first_day.wday
 
-  (first_day..last_day).each do |date|
-    printf("%2d ", date.day)
+#　日付を並べる
+(first_day..last_day).each do |date|
+    
+    # 1桁の数字は右つめにする
+    print "%2d" % date.day + " "
+    # 数字を土曜日で折り返しにする
     if date.saturday?
-      puts "\n"
+        puts "\n"
     end
-  end
-  puts
+end
 end
 
-print_calendar(year, month)
+# 引数で年月日を表示
+#print_calendar(year, month)
+
+# 
