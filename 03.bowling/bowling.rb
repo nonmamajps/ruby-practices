fe932ac265254807418b25e8f43aa8340df5cf42
nonmama <ig.non.noie@gmail.com>
2024-03-15 15:05:52 +0900

    レビュー修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -14,42 +14,34 @@ scores.each do |s|
   end
 end
 
-frames = [] # フレーム毎に分割
-shots.each_slice(2) do |s|
+frames = shots.each_slice(2).to_a do |s|
   frames << s
 end
 
 # ストライクとスペア加算
-point = 0
-frames[0..8].each_with_index do |frame, idx|
-  point += if frame[0] == 10 # strike # [10, 0]この状態のこと
-             if frames[idx + 0][0] == 10 && frames[idx + 1][0] == 10 # 今回と次の投球がストライクだった場合
-               frame.sum +
-                 frames[idx + 1][0] + # 次の投球の1本目
-                 frames[idx + 2][0] # 次の次の投球の1本目
-             else
-               frame.sum +
-                 frames[idx + 1][0] + # 次の投球の1本目
-                 frames[idx + 1][1] # 次の投球の2本目
-             end
+point = frames[0..9].each_with_index.sum do |frame, idx|
+  if frames[9] == 10 || frames[9].sum == 10
+    if frames[9] == 10 && frames[10][0] == 10
+      frames[9][0] + frames[10][0] + frames[11][0]
+    else
+      frames[9].sum + frames[10].sum
+    end
+  end
+  if frame[0] == 10 # strike # [10, 0]この状態のこと
+    if frames[idx + 0][0] == 10 && frames[idx + 1][0] == 10 # 今回と次の投球がストライクだった場合
+      frame.sum +
+        frames[idx + 1][0] + # 次の投球の1本目
+        frames[idx + 2][0] # 次の次の投球の1本目
+    else
+      frame.sum +
+        frames[idx + 1].sum # 次の投球の合計
+    end
 
-           elsif frame.sum == 10 # spare
-             frames[idx + 1][0] + 10
-           else
-             frame.sum
-           end
+  elsif frame.sum == 10 # spare
+    frames[idx + 1][0] + 10
+  else
+    frame.sum
+  end
 end
-
-# 最後のフレーム
-last_frame = frames[9]
-point += if last_frame[0] == 10 || last_frame.sum == 10
-           if last_frame[0] == 10 && frames[10][0] == 10
-             frames[9][0] + frames[10][0] + frames[11][0]
-           else
-             last_frame.sum + frames[10].sum
-           end
-         else
-           last_frame.sum
-         end
 
 puts point
