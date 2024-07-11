58b8261bc3ef75bbdfb89ba99662980059084254
nonmama <ig.non.noie@gmail.com>
2024-07-11 09:42:16 +0900

    メソッド化

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -3,56 +3,74 @@
 
 require 'optparse'
 
+BLANK_CELL = 8
+
 file_name = ARGV
 params = ARGV.getopts('lwc')
 
-
-def no_option(params)
-  params["l"] == false && params["w"] == false && params["c"] == false
+def main(file_name, params)
+  if file_name.empty?
+    standard_input
+  else
+    print_display(file_name, params)
+  end
 end
 
-total_lines = 0
-total_words = 0
-total_bytes = 0
+def standard_input
+  input = $stdin.read
+  print input.count("\n").to_s.rjust(BLANK_CELL)
+  print input.split(/\s+/).size.to_s.rjust(BLANK_CELL)
+  print input.bytesize.to_s.rjust(BLANK_CELL)
+end
 
-file_name.each do |file|
-  sentence = File.read(file)
-  lines = sentence.count("\n")
-  words = sentence.split(/\s+/).size
-  bytes = sentence.bytesize
+def print_display(file_name, params)
+  total_lines = total_words = total_bytes = 0
 
-  if no_option(params)
-    print line_count = lines.to_s.rjust(8)
-    print words_count = words.to_s.rjust(8)
-    print byte_count = bytes.to_s.rjust(8)
+  read_and_count(file_name).each do |result|
+    file = result[:file]
+    lines = result[:lines]
+    words = result[:words]
+    bytes = result[:bytes]
+
+    if no_option(params)
+      print lines.to_s.rjust(BLANK_CELL)
+      print words.to_s.rjust(BLANK_CELL)
+      print bytes.to_s.rjust(BLANK_CELL)
+    else
+      print lines.to_s.rjust(BLANK_CELL) if params['l']
+      print words.to_s.rjust(BLANK_CELL) if params['w']
+      print bytes.to_s.rjust(BLANK_CELL) if params['c']
+    end
     puts " #{file}"
-  else
-  print line_count = lines.to_s.rjust(8) if params["l"]
-  print words_count = words.to_s.rjust(8) if params["w"]
-  print byte_count = bytes.to_s.rjust(8) if params["c"]
-  puts " #{file}"
+
+   total_lines += lines
+   total_words += words
+   total_bytes += bytes
   end
 
-  total_lines += lines
-  total_words += words
-  total_bytes+= bytes
-
+  print_total(total_lines, total_words, total_bytes) if file_name.size >= 2
 end
 
-if file_name.size >= 2
-  print total_lines.to_s.rjust(8)
-  print total_words.to_s.rjust(8)
-  print total_bytes.to_s.rjust(8)
-  puts " total"
+def read_and_count(file_name)
+  file_name.map do |file|
+    sentence = File.read(file)
+    lines = sentence.count("\n")
+    words = sentence.split(/\s+/).size
+    bytes = sentence.bytesize
+
+    { file:file, lines:lines, words:words, bytes:bytes }
+  end
 end
 
-if file_name.empty?
-  input = $stdin.read
-  line_count = input.count("\n").to_s.rjust(8)
-  words_count = input.split(/\s+/).size.to_s.rjust(8)
-  byte_size = input.bytesize.to_s.rjust(8)
-
-  print line_count
-  print words_count
-  print byte_size
+def no_option(params)
+  params['l'] == false && params['w'] == false && params['c'] == false
 end
+
+def print_total(total_lines, total_words, total_bytes)
+  print total_lines.to_s.rjust(BLANK_CELL)
+  print total_words.to_s.rjust(BLANK_CELL)
+  print total_bytes.to_s.rjust(BLANK_CELL)
+  puts ' total'
+end
+
+main(file_name,params)
